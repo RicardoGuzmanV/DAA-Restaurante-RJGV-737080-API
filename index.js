e90399218dfe9bcf87e865ad7af9c5e1ec2742b0
Ricardo Guzman <ricardo.guzman@gmail.com>
2023-03-10 02:25:44 -0600

    Agregando Cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const port = 3000;
 
@@ -19,7 +21,7 @@ const users = [
         name: 'Isai',
         color: 'Azul'
     }
-]
+];
 
 app.get('/', (req, res) => res.send('Restaurant API working!'));
 app.get('/users', (req, res) => res.json(users));
