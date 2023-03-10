9a041abe62dc2480917ba162570f42d5e2f464cf
Ricardo Guzman <ricardo.guzman@gmail.com>
2023-03-10 02:03:33 -0600

    Users info

--- index.js
+++ index.js
@@ -6,4 +6,22 @@ app.use(bodyParser.json());
 
 const port = 3000;
 
+const users = [
+    {
+        name: 'Ricardo',
+        color: 'Rojo'
+    },
+    {
+        name: 'Pedro',
+        color: 'Verde'
+    },
+    {
+        name: 'Isai',
+        color: 'Azul'
+    }
+]
+
+app.get('/', (req, res) => res.send('Restaurant API working!'));
+app.get('/users', (req, res) => res.json(users));
+
 app.listen(port, () => console.log(`Server listening at port 3000`));
