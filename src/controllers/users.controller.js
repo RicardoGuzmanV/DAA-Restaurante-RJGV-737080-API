650547327ac427b1295d0eef2179f5486a17df08
Ricardo Guzman <ricardo.guzman@gmail.com>
2023-04-19 21:10:59 -0600

    o

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,7 +1,34 @@
-const getAllUsers = (req, res) => res.status(200).send('GET /users succeeded');
+const User = require('../models/user.model');
 
-const createUser = (req, res) => res.status(201).send('POST /users succeeded');
+const getAllUsers = async (req, res, next) => {
+    try{
+        const allUsers = await User.find().select('password');
+        return res.status(200).json(allUsers);
+    } catch (error) {
+        return next(error);
+    }
+};
 
-const deleteUser = (req, res) => res.status(204).send();
+const createUser = async (req, res, next) => {
+    try{
+        const newUser = new User({ ...req.body });
+        const insertedUser = await newUser.save();
+        const returnUser = insertedUser.toObject();
+        delete returnUser.password;
+        return res.status(201).json(returnUser);
+    } catch (error) {
+        return next(error);
+    }
+};  
+
+const deleteUser = async (req, res, next) =>{
+ try{
+    const id = req.params.id;
+    await User.findByIdAndDelete(id);
+     res.status(204).send();
+ } catch(error){
+    return next(error)
+ }
+}    
 
 module.exports = { getAllUsers, createUser, deleteUser };
