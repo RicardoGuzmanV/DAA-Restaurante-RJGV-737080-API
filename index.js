5f5ab408a4232728accedf8d5c8ea62569b2dbcb
Ricardo Guzman <ricardo.guzman@gmail.com>
2023-04-19 18:04:29 -0600

    Conexion a mongo

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
-const app = express();
-const port = process.env.port | 3000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 const homeRoutes = require('./src/routes/home.routes');
 const reservationsRoutes = require('./src/routes/reservations.routes');
 const usersRoutes = require('./src/routes/users.routes');
 
+const app = express();
 app.use('/', homeRoutes);
 app.use('/reservations', reservationsRoutes);
 app.use('/users', usersRoutes);
 
-app.listen(port, () => console.log('Server listening at port ' + port));    
+const main = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        app.listen(process.env.PORT, () => console.log('Server listening at port ' + process.env.PORT));
+    } catch(error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+main();
+
