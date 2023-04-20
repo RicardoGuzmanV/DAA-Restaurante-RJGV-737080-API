78a639d1b45bd1ba04f0369f6e5332e7c02d0dcd
Ricardo Guzman <ricardo.guzman@gmail.com>
2023-04-19 20:05:46 -0600

    Mejorando API

--- src/controllers/reservation.controller.js
+++ src/controllers/reservation.controller.js
@@ -1,27 +1,52 @@
 const Reservation = require('../models/reservation.model');
 
-const getAllReservations = async (req, res) => {
+const getAllReservations = async (req, res, next) => {
     try{
         const allReservations = await Reservation.find();
         return res.status(200).json(allReservations);
     } catch (error) {
-        return res.status(400).error(error);
+        return next(error);
     }
 };
 
-const createReservation = async (req, res) => {
+const createReservation = async (req, res, next) => {
     try{
         const newReservation = new Reservation({ ...req.body });
         const insertedReservation = await newReservation.save();
         return res.status(201).json(insertedReservation);
     } catch (error) {
-        return res.status(400).error(error);
+        return next(error);
     }
 };
-const getSingleReservation = (req, res) => res.status(200).send('GET /reservations/:id succeeded');
+const getSingleReservation = async (req, res, next) =>{
+ try{
+    const id = req.params.id;
+    const reservation = await Reservation.findById(id);
+     res.status(200).json(reservation);
+ } catch(error){
+    return next(error)
+ }
+}    
 
-const updateSingleReservation = (req, res) => res.status(200).send('PUT /reservations/:id succeeded');
+const updateSingleReservation = async (req, res, next) =>{
+ try{
+    const id = req.params.id;
+    await Reservation.updateOne({ _id: id }, req.body);
+    const reservation = await Reservation.findById(id);
+     res.status(200).send('PUT /reservations/:id succeeded');
+ } catch(error){
+    return next(error)
+ }
+}    
 
-const deleteReservation = (req, res) => res.status(204).send();
+const deleteReservation = async (req, res, next) =>{
+ try{
+    const id = req.params.id;
+    await Reservation.findByIdAndDelete(id);
+     res.status(204).send();
+ } catch(error){
+    return next(error)
+ }
+}    
 
 module.exports = { getAllReservations, createReservation, getSingleReservation, updateSingleReservation, deleteReservation };
